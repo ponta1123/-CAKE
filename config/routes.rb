42663aa59994a80eedb0e41b9a4f398b1fb721ce
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2022-11-15 13:40:01 +0000

    [Add]エラー確認

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
