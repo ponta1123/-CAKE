c7eadddedbb647844ba233955a4f115d8e0035df
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2022-11-20 09:48:39 +0000

    [error]devise_views

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
 
+root to: 'public/homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
