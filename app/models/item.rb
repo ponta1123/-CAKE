ed7c19c964c3d044c93335e7914970da6634d011
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2022-12-04 09:15:49 +0000

    [add]販売ステータス追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_products, dependent: :destroy
+
 end
--- app/models/order_information.rb
+++ app/models/order_information.rb
@@ -1,4 +1,5 @@
 class OrderInformation < ApplicationRecord
   belongs_to :order_product
   has_many :customers,  dependent: :destroy
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
