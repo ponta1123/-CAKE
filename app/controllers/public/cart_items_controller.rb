899ddf08b058c6398a6deb4999b11d08d8f331b9
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2023-01-19 12:22:31 +0000

    [Finish]完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -19,18 +19,29 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_items = current_customer.cart_items
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
-        cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
-      else
-        @cart_item = CartItem.new(cart_item_params)
-      end
+    # @cart_item = CartItem.new(cart_item_params)
+    # @cart_items = current_customer.cart_items
+    # @cart_items.each do |cart_item|
+    #   if cart_item.item_id == @cart_item.item_id
+    #     new_amount = cart_item.amount + @cart_item.amount
+    #     cart_item.update_attribute(:amount, new_amount)
+    #     @cart_item.delete
+    #   else
+    #     @cart_item = CartItem.new(cart_item_params)
+    #   end
+    # end
+    # @cart_item.save
+    # redirect_to cart_items_path
+
+    @cart_item = CartItem.find_by(item_id:cart_item_params[:item_id],customer_id:current_customer.id)
+    if @cart_item
+      #CartItem.exists?(item_id:cart_item_params[:item_id])
+       new_amount = cart_item_params[:amount].to_i + @cart_item.amount
+       @cart_item.update_attribute(:amount, new_amount)
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.save
     end
-    @cart_item.save
     redirect_to cart_items_path
   end
 
